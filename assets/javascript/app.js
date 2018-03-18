0e8dac4bd28906d9113b018ab2c7981c7b22fc4c
Daniel Foster <danieljfoster87@gmail.com>
2018-03-18 13:31:50 -0400

    uber api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,15 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+function getUber() {
+  $.ajax({
+    url:
+      "https://api.uber.com/v1.2/estimates/time?start_latitude=37.7752315&start_longitude=-122.418075&end_latitude=37.7752415&end_longitude=-122.518075",
+    headers: { Authorization: "Token 7hStE0iarmRfSQDj_-N6JqKy0X0PhCus3jEh20e0" }
+  })
+    .then(function(res) {
+      console.log(res);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+getUber();
